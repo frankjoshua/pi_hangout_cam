c91fc7ac7c09a5dccb3806e67d18d8b111fdd078
frankjoshua <frankjoshua@gmail.com>
2014-11-30 21:13:56 -0600

    Updated arduino to work with Sabertooth driver

--- arduino_serial/arduino_serial.ino
+++ arduino_serial/arduino_serial.ino
@@ -1,29 +1,20 @@
-#include <Servo.h>
+#include <SoftwareSerial.h>
+#include <SabertoothSimplified.h>
 
-//Setup servos
-Servo tiltServo;
-#define PIN_TILT_SERVO 8
-Servo panServo;
-#define PIN_PAN_SERVO 7
+#define FULL_SPEED 70
+//Define motors
+#define LEFT 2
+#define RIGHT 1
+#define MOTOR_PIN 6
+SoftwareSerial SWSerial(NOT_A_PIN, MOTOR_PIN); // RX on no pin (unused), TX on pin 11 (to S1).
+SabertoothSimplified ST(SWSerial); // Use SWSerial as the serial port.
 
-//Input budder for serial
+//Input buffer for serial
 char in[2] = {'a', '\0'};
-
-#define STEPS 15
-#define MAX_PAN 165
-#define MAX_TILT 165
-#define MIN_PAN 15
-#define MIN_TILT 15
-
-int mTilt = 90;
-int mPan = 90;
 
 void setup() {
   Serial.begin(115200);
-  tiltServo.attach(PIN_TILT_SERVO);
-  tiltServo.write(mTilt);
-  panServo.attach(PIN_PAN_SERVO);
-  panServo.write(mPan);
+
 }
 
 void loop() {
@@ -32,27 +23,32 @@ void loop() {
    in[0] = Serial.read();
    //Serial.print(in);
    if(strcmp(in, "u") == 0){
-     mTilt += STEPS;
+    
    } else if(strcmp(in, "d") == 0){
-     mTilt -= STEPS;
+
    } else if(strcmp(in, "l") == 0){
-     mPan += STEPS;
+      left();
    } else if(strcmp(in, "r") == 0){
-     mPan -= STEPS;
+      right();
    }
-   //Check for out of range values
-   if(mTilt > MAX_TILT){
-     mTilt -= STEPS;
-   } else if(mTilt < MIN_TILT){
-     mTilt += STEPS; 
-   }
-   if(mPan > MAX_PAN){
-     mPan -= STEPS; 
-   } else if(mPan < MIN_PAN){
-     mPan += STEPS; 
-   }
-   //Move servos
-   tiltServo.write(mTilt);
-   panServo.write(mPan);
+
   }
+}
+
+void fullStop(){
+  delay(500); 
+  ST.motor(RIGHT, 0);
+  ST.motor(LEFT, 0);
+}
+
+void right(){
+  ST.motor(RIGHT, FULL_SPEED);
+  ST.motor(LEFT, -FULL_SPEED);
+  fullStop();
+}
+
+void left(){
+  ST.motor(RIGHT, -FULL_SPEED);
+  ST.motor(LEFT, FULL_SPEED);
+  fullStop();
 }
